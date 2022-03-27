4d2dd6ee9c7393afda0a33d6a6fd65d64ff216f5
MarwenKRICHEN <marwenkrichen@insat.u-carthage.tn>
2022-03-27 23:40:52 +0100

    sign in page

--- app/src/main/java/com/example/dmerjimirror/ui/authentication/SignInFragment.kt
+++ app/src/main/java/com/example/dmerjimirror/ui/authentication/SignInFragment.kt
@@ -1,6 +1,37 @@
 package com.example.dmerjimirror.ui.authentication
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.dmerjimirror.databinding.FragmentDashboardBinding
+import com.example.dmerjimirror.databinding.FragmentSignInBinding
+import com.example.dmerjimirror.ui.dashboard.DashboardViewModel
 
 class SignInFragment: Fragment() {
+    private var _binding: FragmentSignInBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentSignInBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
